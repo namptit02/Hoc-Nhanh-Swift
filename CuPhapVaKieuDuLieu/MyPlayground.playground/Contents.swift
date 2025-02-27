3472d19b8f234d72f429d58121d8ce72c1eddd8b
namptit02 <namhoaq2k2@gmail.com>
2025-02-27 17:12:28 +0700

    feat: bien va kieu du lieu

--- CuPhapVaKieuDuLieu/MyPlayground.playground/Contents.swift
+++ CuPhapVaKieuDuLieu/MyPlayground.playground/Contents.swift
@@ -2,3 +2,15 @@ import UIKit
 
 var greeting = "Hello, playground"
 //Bài 1: Khai báo và sử dụng biến Tạo các biến để lưu thông tin cá nhân (tên, tuổi, chiều cao, cân nặng, địa chỉ) và in ra màn hình theo định dạng hồ sơ.
+var hoTen: String = "Trần Văn A"
+var tuoi: Int = 24
+var chieuCao: Float = 178
+var canNang: Float = 65
+var diaChi: String = "Hà Nội"
+print("Họ tên: \(hoTen)")
+print("Tuổi: \(tuoi)")
+print("Chiều cao: \(chieuCao) cm")
+print("Cân nặng: \(canNang) kg")
+
+
+print("Họ tên: \(hoTen) ,chiều cao: \(chieuCao) cm , tuổi: \(tuoi)")
