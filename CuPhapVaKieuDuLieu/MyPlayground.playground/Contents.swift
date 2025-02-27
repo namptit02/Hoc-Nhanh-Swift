7d4395c566712410aecd4c43558205af47a724d5
namptit02 <namhoaq2k2@gmail.com>
2025-02-27 16:47:53 +0700

    Bai1

---
+++ CuPhapVaKieuDuLieu/MyPlayground.playground/Contents.swift
@@ -0,0 +1,4 @@
+import UIKit
+
+var greeting = "Hello, playground"
+//Bài 1: Khai báo và sử dụng biến Tạo các biến để lưu thông tin cá nhân (tên, tuổi, chiều cao, cân nặng, địa chỉ) và in ra màn hình theo định dạng hồ sơ.
